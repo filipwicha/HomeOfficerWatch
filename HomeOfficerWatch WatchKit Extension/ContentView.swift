b7890650f851531f712a92a0b36e377056700b94
filipwicha <33604199+filipwicha@users.noreply.github.com>
2020-06-27 13:00:39 +0200

    app for apple watch ready

--- HomeOfficerWatch WatchKit Extension/ContentView.swift
+++ HomeOfficerWatch WatchKit Extension/ContentView.swift
@@ -16,47 +16,43 @@ struct ContentView: View {
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     
     var body: some View {
-        ZStack {
-            Circle()
-                    .stroke(lineWidth: 35.0)
-                    .opacity(0.3)
-                    .foregroundColor(self.basicViewModel.timeStamp.relay == true ? Color.green  : Color.red)
-                    .frame(width: 300, height: 300)
+        VStack{
+            Button(action: {self.basicViewModel.change()}){
                 
-            Circle()
-                    .trim(from: 0.0, to: CGFloat(min(self.basicViewModel.time.percentOfRegularPost, 1.0)))
-                    .stroke(style: StrokeStyle(lineWidth: 35.0, lineCap: .round, lineJoin: .round))
-                    .foregroundColor(self.basicViewModel.timeStamp.relay == true ? Color.green  : Color.red)
-                    .rotationEffect(Angle(degrees: 270.0))
-                    .animation(.linear)
-                    .frame(width: 300, height: 300)
-            VStack{
-                Text(self.basicViewModel.timeStamp.uuid)
-                    .opacity(0.5)
-                    .font(.title)
-                Text(self.basicViewModel.timeStamp.relay ? "You've been working for" : "Time your have worked")
-                Text(self.basicViewModel.time.differenceString)
-                        .font(.largeTitle)
+                VStack{
+                    Text(self.basicViewModel.timeStamp.relay ? "Stop working" : "Start working")
+                        .foregroundColor(self.basicViewModel.timeStamp.relay ? .red : .green )
+                    Text(self.basicViewModel.time.differenceString)
+                        .font(.system(size: 15))
                         .bold()
                         .onReceive(timer) { _ in
                             if(self.basicViewModel.timeStamp.relay == true){
                                 self.basicViewModel.changeTime()
                             }
-                        }
-                Button(action: {self.basicViewModel.change()}){
-                    VStack{
-                        Text(self.basicViewModel.timeStamp.relay ? "Click to stop working" : "Click to start working")
-                        .foregroundColor(self.basicViewModel.timeStamp.relay ? .red : .green )
-                    Text("💻").font(.title)
                     }
-                
-                    }
-                }
+                }.padding()
             }
             
-        .frame(width: 340, height: 340)
+            ZStack {
+                Circle()
+                    .stroke(lineWidth: 9.0)
+                    .opacity(0.3)
+                    .foregroundColor(self.basicViewModel.timeStamp.relay == true ? Color.green  : Color.red)
+//                    .frame(width: 140, height: 140)
+                
+                Circle()
+                    .trim(from: 0.0, to: CGFloat(min(self.basicViewModel.time.percentOfRegularPost, 1.0)))
+                    .stroke(style: StrokeStyle(lineWidth: 9.0, lineCap: .round, lineJoin: .round))
+                    .foregroundColor(self.basicViewModel.timeStamp.relay == true ? Color.green  : Color.red)
+                    .rotationEffect(Angle(degrees: 270.0))
+                    .animation(.linear)
+//                    .frame(width: 140, height: 140)
+                Text("💻").font(.system(size: 50))
+            }.frame(width: 100, height: 100)
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
